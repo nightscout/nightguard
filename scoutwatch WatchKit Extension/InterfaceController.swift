1f8e6d5594ec0332a38cf6750a42e2454d6240ad
dirk <h3rmanns@gmail.com>
2015-11-20 15:22:48 +0100

    Initial Commit

---
+++ scoutwatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  scoutwatch WatchKit Extension
+//
+//  Created by Dirk Hermanns on 20.11.15.
+//  Copyright © 2015 private. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ scoutwatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scoutwatch
+//
+//  Created by Dirk Hermanns on 20.11.15.
+//  Copyright © 2015 private. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
