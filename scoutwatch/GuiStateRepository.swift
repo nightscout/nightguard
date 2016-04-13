9bffc24956c6ea04ccc5449c8f697d587ffc8483
dirk <h3rmanns@gmail.com>
2016-04-13 23:08:50 +0200

    Storage of the volume position is not necessary any more

--- scoutwatch/GuiStateRepository.swift
+++ scoutwatch/GuiStateRepository.swift
@@ -8,28 +8,10 @@
 
 import Foundation
 
-// Stores/Restores the state of the GUI. That is the Volume-Slider Position
-// and the screenlock-switch.
+// Stores/Restores the state of the GUI. That is right now the screenlock-switch.
 class GuiStateRepository {
     
     static let singleton = GuiStateRepository()
-    
-    func storeVolumeSliderPosition(let currentPosition : Float) {
-        let defaults = NSUserDefaults(suiteName: AppConstants.APP_GROUP_ID)
-        defaults!.setObject(currentPosition, forKey: "volumeSliderPosition")
-    }
-    
-    func loadVolumeSliderPosition() -> Float {
-        guard let defaults = NSUserDefaults(suiteName: AppConstants.APP_GROUP_ID) else {
-            // Nothing has been stored before => return slider position 0
-            return 0
-        }
-        
-        guard let currentPosition = defaults.objectForKey("volumeSliderPosition") as? Float else {
-            return 0
-        }
-        return currentPosition
-    }
 
     func storeScreenlockSwitchState(let isActivated : Bool) {
         let defaults = NSUserDefaults(suiteName: AppConstants.APP_GROUP_ID)
