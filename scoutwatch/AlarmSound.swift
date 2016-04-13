1c5e03e77b687c2bd65e0869935a3e043386a6d4
dirk <h3rmanns@gmail.com>
2016-04-13 23:12:52 +0200

    Removed obsolete methods due to the switch to the MPVolumeView.

--- scoutwatch/AlarmSound.swift
+++ scoutwatch/AlarmSound.swift
@@ -36,37 +36,7 @@ class AlarmSound {
     static func unmuteVolume() {
         mute = false;
         self.audioPlayer.volume = volumeBeforeMute
-    }
-    
-    /*
-     * Changes the alarm sound volume. 1.0 is 100% volume. 0.0 is 0% volume.
-     */
-    static func changeAlarmVolume(volume : Float) {
-        self.audioPlayer.volume = volume
-        
-        // change the system volume, so that sound will be played whatever system
-        // settings you have
-        let volumeView = MPVolumeView()
-            
-        for view in volumeView.subviews {
-            if (NSStringFromClass(view.classForCoder) == "MPVolumeSlider") {
-                let slider = view as! UISlider
-                slider.setValue(volume, animated: false)
-            }
-        }
-    }
-    
-    static func getAlarmVolume() -> Float {
-        let volumeView = MPVolumeView()
-        
-        for view in volumeView.subviews {
-            if (NSStringFromClass(view.classForCoder) == "MPVolumeSlider") {
-                let slider = view as! UISlider
-                return slider.value
-            }
-        }
-        return 0
-    }
+    }	
     
     static func stop() {
         
