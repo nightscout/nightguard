b716e1f534a1b846669cf5eca837bb16d0c06a92
dirk <h3rmanns@gmail.com>
2016-05-22 22:04:09 +0200

    Fixed the 'spinning wheel of death' for the watchkit app

--- scoutwatch WatchKit Extension/AppConstants.swift
+++ scoutwatch WatchKit Extension/AppConstants.swift
@@ -10,5 +10,5 @@ import Foundation
 
 // Class that contains general constants used in different classes
 class AppConstants {
-    internal static let APP_GROUP_ID = "group.de.dhe.scoutwatch"
+    internal static let APP_GROUP_ID = "group.de.my-wan.dhe.nightguard"
 }
---
+++ scoutwatch WatchKit Extension/TestController.swift
@@ -0,0 +1,13 @@
+//
+//  TestController.swift
+//  nightguard
+//
+//  Created by Dirk Hermanns on 22.05.16.
+//  Copyright © 2016 private. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class TestController : WKInterfaceController {
+}
