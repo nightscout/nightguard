bafcb27da4ae03bd13d5ed71e38345ca306e023b
dirk <h3rmanns@gmail.com>
2020-09-10 20:05:26 +0200

    Preparations to switch the watch-app to swiftui

--- nightguard WatchKit Extension/HostingController.swift
+++ nightguard WatchKit Extension/HostingController.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 private. All rights reserved.
 //
 
+import WatchKit
 import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
+
+struct HostingController_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
