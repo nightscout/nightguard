3c18bd672231b77c626369565463db3e75084d26
Sebastian Deisel <sebastian.deisel@loyjoy.com>
2022-03-09 19:35:59 +0100

    fixed cellselection

--- nightguard/PrefsViewController.swift
+++ nightguard/PrefsViewController.swift
@@ -205,20 +205,21 @@ class PrefsViewController: CustomFormViewController {
             }
         
             <<< SwitchRow() { row in
-                row.disabled = Condition(booleanLiteral: !AppleHealthService.singleton.isAuthorized())
                 row.title = NSLocalizedString("Synchronize with Apple Health", comment: "Label for Apple Health synchronization")
                 row.value = AppleHealthService.singleton.isAuthorized()
             }.onChange { row in
-                if (row.value ?? false) {
+                if (AppleHealthService.singleton.isAuthorized()) {
+                    let title:String = NSLocalizedString("Synchronize with Apple Health", comment: "Label for Apple Health synchronization")
+                    let message: String = NSLocalizedString("Revoke access in Apple Health", comment: "Label for Apple Health access revocation")
+                    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                    
+                    self.present(alert, animated: true, completion: nil)
+                    Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: { _ in alert.dismiss(animated: true, completion: nil)} )
+
+                    row.value = AppleHealthService.singleton.isAuthorized()
+                } else {
                     AppleHealthService.singleton.requestAuthorization()
                 }
-            }.onCellSelection { _, row in
-                let title:String = NSLocalizedString("Synchronize with Apple Health", comment: "Label for Apple Health synchronization")
-                let message: String = NSLocalizedString("Revoke access in Apple Health", comment: "Label for Apple Health access revocation")
-                let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-
-                self.present(alert, animated: true, completion: nil)
-                Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: { _ in alert.dismiss(animated: true, completion: nil)} )
             }
             
             <<< LabelRow() { row in
