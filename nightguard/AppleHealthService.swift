6125d0737a88113d9fc03ea4dbbd21fc0ea5c8d0
Sebastian Deisel <sebastian.deisel@loyjoy.com>
2022-03-11 21:02:11 +0100

    fix for mmol unit

--- nightguard/AppleHealthService.swift
+++ nightguard/AppleHealthService.swift
@@ -49,8 +49,10 @@ class AppleHealthService: NSObject {
         let hkQuantitySamples: [HKQuantitySample] = bgData
             .filter{ bloodGlucose in bloodGlucose.date > lastSyncDate }
             .compactMap{ bloodGlucose in
+                let convertedValue: Float = UnitsConverter.mgdlToDisplayUnits(bloodGlucose.value)
+                
                 let date: Date = bloodGlucose.date
-                let value: Double = Double(bloodGlucose.value)
+                let value: Double = Double(convertedValue)
 
                 return HKQuantitySample(
                     type: getHkQuantityType(),
