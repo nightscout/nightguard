5ddaad15ab3142f81457327df66fa6f49ae43f9b
dhermanns <h3rmanns@gmail.com>
2026-02-02 20:49:23 +0100

    Fixed a chart repaint problem.

--- nightguard/AppDelegate.swift
+++ nightguard/AppDelegate.swift
@@ -269,6 +269,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        UserDefaultsRepository.currentTab.value = .main
     }
     
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
