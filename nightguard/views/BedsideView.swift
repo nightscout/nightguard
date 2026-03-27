0c8f3126e2e43cb82c27d434acbe6c51d4db96e0
dhermanns <h3rmanns@gmail.com>
2026-03-27 16:56:27 +0100

    #350: Improved BedsideView with even bigger bgvalues. Have fun, Frank ;)

--- nightguard/views/BedsideView.swift
+++ nightguard/views/BedsideView.swift
@@ -19,80 +19,17 @@ struct BedsideView: View {
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
+        GeometryReader { geometry in
+            ZStack {
+                Color.black.ignoresSafeArea()
 
-            VStack(spacing: 20) {
-                Spacer()
-
-                // Blood glucose value
-                Text(bgValue)
-                    .font(.system(size: 150, weight: .bold))
-                    .foregroundColor(bgColor)
-                    .minimumScaleFactor(0.5)
-
-                // Delta and arrows
-                HStack(spacing: 10) {
-                    Text(deltaValue)
-                        .font(.system(size: 50))
-                        .foregroundColor(deltaColor)
-
-                    Text(deltaArrows)
-                        .font(.system(size: 50))
-                        .foregroundColor(deltaColor)
+                if isLandscape(size: geometry.size) {
+                    landscapeContent(size: geometry.size)
+                } else {
+                    portraitContent
                 }
 
-                // Last update time
-                Text(lastUpdateValue)
-                    .font(.system(size: 35))
-                    .foregroundColor(lastUpdateColor)
-
-                // Current time
-                Text(currentTime)
-                    .font(.system(size: 30))
-                    .foregroundColor(.white)
-
-                Spacer()
-
-                // Snooze info
-                if let snoozeInfo = snoozeInfo {
-                    Text(snoozeInfo)
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                        .padding(.bottom, 10)
-                }
-
-                // Alert info
-                if let alertReason = alertReason {
-                    Text(alertReason)
-                        .font(.system(size: 30))
-                        .foregroundColor(alertColor)
-                        .padding(.bottom, 20)
-                }
-
-                Spacer()
-            }
-
-            // Close button
-            VStack {
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        dismiss()
-                    }) {
-                        Image("close")
-                            .resizable()
-                            .renderingMode(.template)
-                            .frame(width: 24, height: 24)
-                            .foregroundColor(.white)
-                            .padding(12)
-                            .background(Color(UIColor.darkGray.withAlphaComponent(0.3)))
-                            .clipShape(Circle())
-                    }
-                    .padding()
-                    .accessibilityIdentifier("fullscreenCloseButton")
-                }
-                Spacer()
+                closeButtonOverlay
             }
         }
         .statusBar(hidden: true)
@@ -111,6 +48,68 @@ struct BedsideView: View {
     }
 
     // MARK: - Computed Properties
+
+    private var portraitContent: some View {
+        VStack(spacing: 20) {
+            Spacer()
+
+            Text(bgValue)
+                .font(.system(size: 190, weight: .bold))
+                .foregroundColor(bgColor)
+                .minimumScaleFactor(0.45)
+
+            deltaStack(fontSize: 74, spacing: 12)
+
+            Text(lastUpdateValue)
+                .font(.system(size: 50))
+                .foregroundColor(lastUpdateColor)
+
+            Text(currentTime)
+                .font(.system(size: 46))
+                .foregroundColor(.white)
+
+            Spacer()
+
+            if let snoozeInfo = snoozeInfo {
+                Text(snoozeInfo)
+                    .font(.system(size: 44))
+                    .foregroundColor(.white)
+                    .padding(.bottom, 10)
+            }
+
+            if let alertReason = alertReason {
+                Text(alertReason)
+                    .font(.system(size: 44))
+                    .foregroundColor(alertColor)
+                    .padding(.bottom, 20)
+            }
+
+            Spacer()
+        }
+    }
+
+    private var closeButtonOverlay: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image("close")
+                        .resizable()
+                        .renderingMode(.template)
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.white)
+                        .padding(12)
+                        .background(Color(UIColor.darkGray.withAlphaComponent(0.3)))
+                        .clipShape(Circle())
+                }
+                .padding()
+                .accessibilityIdentifier("fullscreenCloseButton")
+            }
+            Spacer()
+        }
+    }
 
     private var bgValue: String {
         guard let data = currentNightscoutData else { return "---" }
@@ -149,6 +148,73 @@ struct BedsideView: View {
     }
 
     // MARK: - Private Methods
+
+    @ViewBuilder
+    private func landscapeContent(size: CGSize) -> some View {
+        let infoColumnWidth = min(max(size.width * 0.24, 210), 290)
+        let horizontalPadding: CGFloat = 24
+        let interColumnSpacing: CGFloat = 24
+        let bgFontSize = min(size.height * 0.96, (size.width - infoColumnWidth - (horizontalPadding * 2) - interColumnSpacing) * 0.92)
+
+        HStack(spacing: interColumnSpacing) {
+            Text(bgValue)
+                .font(.system(size: max(bgFontSize, 210), weight: .bold))
+                .foregroundColor(bgColor)
+                .minimumScaleFactor(0.22)
+                .lineLimit(1)
+            .frame(maxHeight: .infinity, alignment: .center)
+            .layoutPriority(1)
+
+            VStack(alignment: .leading, spacing: 14) {
+                Spacer()
+
+                deltaStack(fontSize: 72, spacing: 12)
+
+                Text(lastUpdateValue)
+                    .font(.system(size: 46, weight: .medium))
+                    .foregroundColor(lastUpdateColor)
+
+                Text(currentTime)
+                    .font(.system(size: 42))
+                    .foregroundColor(.white)
+
+                if let snoozeInfo = snoozeInfo {
+                    Text(snoozeInfo)
+                        .font(.system(size: 38))
+                        .foregroundColor(.white)
+                }
+
+                if let alertReason = alertReason {
+                    Text(alertReason)
+                        .font(.system(size: 38, weight: .medium))
+                        .foregroundColor(alertColor)
+                }
+
+                Spacer()
+            }
+            .frame(width: infoColumnWidth)
+            .frame(maxHeight: .infinity, alignment: .leading)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .padding(.horizontal, horizontalPadding)
+        .padding(.vertical, 28)
+    }
+
+    private func deltaStack(fontSize: CGFloat, spacing: CGFloat) -> some View {
+        HStack(spacing: spacing) {
+            Text(deltaValue)
+                .font(.system(size: fontSize))
+                .foregroundColor(deltaColor)
+
+            Text(deltaArrows)
+                .font(.system(size: fontSize))
+                .foregroundColor(deltaColor)
+        }
+    }
+
+    private func isLandscape(size: CGSize) -> Bool {
+        size.width > size.height
+    }
 
     private func updateCurrentTime() {
         let formatter = DateFormatter()
